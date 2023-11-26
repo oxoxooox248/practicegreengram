07b2511c2071a2cc8435045163d81076680d6dcb
oxoxooox248 <xoxoxxxo248@gmail.com>
2023-11-26 16:20:22 +0900

    1차 완료

--- src/main/java/practicegreengram/feed/FeedMapper.java
+++ src/main/java/practicegreengram/feed/FeedMapper.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 @Mapper
 public interface FeedMapper {
-    int insFeed(FeedInsProcDto dto);
-    List<FeedSelVo> selFeedAll(FeedSelDto dto);
-    Integer selFeed(FeedDelDto dto);
-    int delFeed(int ifeed);
-    int insFeedPics(FeedInsProcDto dto);
-    List<FeedPicsVo> selFeedPics(List<Integer> ifeedList);
-    int delPicsByIfeed(int ifeed);
-    int insFeedFav(FeedFavDto dto);
-    int delFeedFav(FeedFavDto dto);
-    int delFavByIfeed(int feed);
-    int insFeedComment(FeedCommentInsProcDto dto);
-    List<FeedCommentSelVo> selCommentAll(FeedCommentSelDto dto);
-    int delFeedComment(FeedCommentDelDto dto);
-    int delCommentByIfeed(int ifeed);
+    int insFeed(FeedInsProcDto dto);//피드 작성
+    List<FeedSelVo> selFeedAll(FeedSelDto dto);//피드 리스트
+    Integer selFeed(FeedDelDto dto);//피드 삭제 시 확인하는 select문
+    int delFeed(int ifeed);//피드 삭제
+    int insFeedPics(FeedInsProcDto dto);//피드 작성 시 실행되는 insert문(사진 추가)
+    List<FeedPicsVo> selFeedPics(List<Integer> ifeedList);//피드 리스트 실행 시 해당 피드들의 사진들
+    int delPicsByIfeed(int ifeed);//피드 삭제 시 해당 피드의 사진들 삭제
+    int insFeedFav(FeedFavDto dto);//좋아요 등록
+    int delFeedFav(FeedFavDto dto);//좋아요 취소
+    int delFavByIfeed(int feed);//피드 삭제 시 해당 피드의 좋아요 삭제
+    int insFeedComment(FeedCommentInsProcDto dto);//댓글 작성
+    List<FeedCommentSelVo> selCommentAll(FeedCommentSelDto dto);//댓글 더보기
+    int delFeedComment(FeedCommentDelDto dto);//댓글 삭제
+    int delCommentByIfeed(int ifeed);//피드 삭제 시 해당 피드의 댓글들 삭제
 
 }
